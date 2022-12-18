8450ed1552dc112d5d6fcb13f12053156e306aa3
Farhan <farhan.muhammad.tif422@polban.ac.id>
2022-12-18 17:55:49 +0700

    module header and score limitation

--- gameplay.c
+++ gameplay.c
@@ -2,6 +2,11 @@
 #define N 8
 
 /*
+    Author : Farhan Muhammad Luthfi
+    Date   : 18/12/2022
+*/
+
+/*
 Now for papan, I need some unicode to print the edge, how...
 
 OK found some unicode for box shape from here
@@ -48,6 +53,13 @@ int hX = 70;
 int hY = 5;
 int wkt;
 
+/*
+Deskripsi	: Modul ini sebagai modul utama untuk bermain. Dari sini tampilan akan direset dan berubah menjadi tampilan untuk bermain yang berisi papan catur, timer, skor,
+dan riwayat gerakan player.
+I.S	: Game belum dijalankan
+F.S	: Game dijalankan
+*/
+
 void PlayGame(Player pl) {
     // Hide cursor
     printf("\e[?25l");
@@ -64,11 +76,11 @@ void PlayGame(Player pl) {
     // Hitung mundur
     int x;
     for (x=5;x>=1;--x) {
-        invgotoxy(15,2);
+        invGotoxy(15,2);
         printf("Permainan di mulai dalam : %d", x);
         Sleep(1000);
     }
-    invgotoxy(15,2);
+    invGotoxy(15,2);
     printf("                                              ");
 
     tampilMisc(pl);
@@ -82,11 +94,24 @@ void PlayGame(Player pl) {
     }
 }
 
-void invgotoxy(int x, int y) {
+/*
+Deskripsi	: Modul untuk memindahkan posisi cursor pada console. X dan y dibalik untuk kebutuhan papan.
+I.S	: Cursor berada di titik asal
+F.S	: Cursor berpindah ke titik yang diinput
+*/
+
+void invGotoxy(int x, int y) {
     printf("\033[%d;%dH", y, x);
     // Inverted because I like the x to be vertically and y to be horizontally
     // Also use 2:1 ratio pls idk why but yeah lmao google it
 }
+
+/*
+Deskripsi	: Modul ini digunakan untuk menampilkan papan. Papan akan diprint ulang setiap kali pemain melakukan gerakan untuk memutakhirkan papan sesuai dengan kondisi
+atau state masing-masing petak. Dalam modul ini akan dilakukan pengecekan status dari setiap petak lalu melakukan pencetakan tergantung status tersebut.
+I.S	: Papan catur tidak tampil
+F.S	: Papan catur ditampilkan
+*/
 
 void tampilPapan(Player pl) {
     // Inisialisasi var
@@ -97,20 +122,20 @@ void tampilPapan(Player pl) {
     papan[Px][Py].status = 'P';
 
     // Tampilan papan
-    invgotoxy(15,5);
+    invGotoxy(15,5);
     printf(" _______________________________________________");
     for (i=7;i<=21;i+=2) {
-        invgotoxy(15,i);
+        invGotoxy(15,i);
         printf("|-----|-----|-----|-----|-----|-----|-----|-----|");
     }
     int x=0;
     int y;
     for (i=6;i<6+(N*2-1);i+=2){
         y = 0;
-        invgotoxy(15,i);
+        invGotoxy(15,i);
         printf("|     |     |     |     |     |     |     |     |");
         for (j=3;j<3+(N*6-1);j+=6) {
-            invgotoxy(15+j,i);
+            invGotoxy(15+j,i);
             if (papan[x][y].status=='O') printf("");
             else if (papan[x][y].status=='X') printf("X");
             else if (papan[x][y].status=='H') printf("H");
@@ -123,25 +148,38 @@ void tampilPapan(Player pl) {
     fflush(stdin);
 }
 
+/*
+Deskripsi	: Modul untuk mengatur posisi awal x dan y player pada papan. Data x dan y dari struct Player diubah lalu melakukan perubahan pada papan sesuai lokasi player
+pada papan. Juga untuk pengaturan timer yang diinginkan, dan inisialisasi skor.
+I.S	: Posisi awal player belum diketahui
+F.S	: Posisi awal player sudah diketahui, timer dan skor sudah diinisialisasi.
+*/
+
 void initializePlayer(Player *pl) {
     int x,y;
     do {
-        invgotoxy(15,2);
-        printf("                                                                             ");
-        invgotoxy(15,2);
+        invGotoxy(15,2);
         printf("Masukkan koordinat petak pertama (x,y) (Format masukkan 'x y' tanpa petik) = ");
         scanf("%d %d", &x, &y);
         fflush(stdin);
         if (x<1 || x>8 || y<1 || y>8) {
-            invgotoxy(15,3);
-            printf("Masukkan koordinat yang tepat");
+            system("cls");
+            invGotoxy(15,3);
+            printf("Masukkan koordinat yang benar!");
         }
     } while (x<1 || x>8 || y<1 || y>8);
-    
-    invgotoxy(15,3);
-    printf("Masukkan waktu batas tiap gerakan (dalam detik) = ");
-    scanf("%d", &wkt);
-    fflush(stdin);
+    system("cls");
+    do {
+        invGotoxy(15,3);
+        printf("Masukkan waktu batas tiap gerakan (dalam detik) = ");
+        scanf("%d", &wkt);
+        fflush(stdin);
+        if (wkt==0 || wkt>30) {
+            system("cls");
+            invGotoxy(15,4);
+            printf("Masukkan waktu yang benar!");
+        }
+    } while (wkt==0 || wkt>30);
     pl->x = y-1;
     pl->y = x-1;
     iX = pl->x;
@@ -151,17 +189,29 @@ void initializePlayer(Player *pl) {
     tampilPapan(*pl);
 }
 
+/*
+Deskripsi	: Menampilkan skor
+I.S	: Skor belum diupdate.
+F.S	: Skor terupdate
+*/
+
 void tampilMisc(Player pl) {
-    invgotoxy(15,23);
+    invGotoxy(15,23);
     printf("Skor = %d", pl.skor);
 }
+
+/*
+Deskripsi	: Memvalidasi apakah petak yang dituju merupakan langkah kuda.
+I.S	: Tidak diketahui status validasi petak.
+F.S	: Diketahui status validasi petak.
+*/
 
 /*
 Validasi
 
 1. possible move
-x+1,y+2 
-x-1,y+2 
+x+1,y+2
+x-1,y+2
 x+2,y+1
 x+2,y-1
 x+1,y-2
@@ -184,6 +234,12 @@ bool validate(int ix, int iy, int x, int y) {
     return false;
 }
 
+/*
+Deskripsi	: Memvalidasi apakah masih ada petak yang bisa ditempati oleh pemain.
+I.S	: Tidak diketahui status evaluasi petak.
+F.S	: Diketahui status evaluasi petak.
+*/
+
 // True : lanjut
 // False : kalah
 
@@ -195,51 +251,63 @@ bool evaluate(int x, int y) {
         if (x+possibleMove[i][0]>=0 && x+possibleMove[i][0]<=7 && y+possibleMove[i][1]>=0 && y+possibleMove[i][1]<=7) {
             if (eks[x+possibleMove[i][0]][y+possibleMove[i][1]].status!='X') {
                 return true;
-            } 
+            }
         }
     }
     return false;
 }
 
-void History() {
+/*
+Deskripsi	: Menampilkan jejak langkah pemain selama permainan berlangsung.
+I.S	: Jejak terbaru belum ditampilkan.
+F.S	: Jejak terbaru sudah ditampilkan.
+*/
+
+void history() {
     if (hY==25){
         hY=5;
         hX+=11;
     }
-    invgotoxy(hX, hY);
+    invGotoxy(hX, hY);
     printf("X: %d Y: %d",iY+1, iX+1);
     hY++;
 }
+
+/*
+Deskripsi	: Menyimpan hasil permainan pemain setelah permainan selesai dengan kondisi baik menang maupun kalah.
+I.S	: Jejak terbaru belum ditampilkan.
+F.S	: Jejak terbaru sudah ditampilkan.
+*/
 
 void Move(Player *pl){
     int x,y;
     bool beres = false;
     bool menang;
-    do { 
+    do {
         time_t start_time = clock();
         for( ;clock() - start_time <= wkt*CLOCKS_PER_SEC; )
-        {   
-            invgotoxy(15,3);
+        {
+            invGotoxy(15,3);
             printf("Timer : %d", (clock()-start_time)/1000);
             if(kbhit() > 0)  // some key is pressed
             {
                 x = pl->x;
                 y = pl->y;
                 char c = getch();
-                switch(c) { // the real value
+                switch(c) {
                     case '\r':
                         if (!validate(iX,iY,x,y) || eks[x][y].status=='X') {
-                            invgotoxy(15, 4);
+                            invGotoxy(15, 4);
                             printf("Ini bukan langkah kuda yang valid!");
                         } else {
                             papan[iX][iY].status = 'X';
                             eks[iX][iY].status = 'X';
 
-                            History();
+                            history();
 
-                            invgotoxy(15, 3);
+                            invGotoxy(15, 3);
                             printf("                                     ");
-                            invgotoxy(15, 4);
+                            invGotoxy(15, 4);
                             printf("                                     ");
 
                             iX = x;
@@ -302,34 +370,40 @@ void Move(Player *pl){
         next_loop: ;
     } while(!beres);
     if (!menang) {
-        invgotoxy(15,2);
+        invGotoxy(15,2);
         printf("                                              ");
-        invgotoxy(15,3);
+        invGotoxy(15,3);
         printf("                                              ");
-        invgotoxy(15,2);
+        invGotoxy(15,2);
         printf("Anda kalah, wek");
     } else {
-        invgotoxy(15,2);
+        invGotoxy(15,2);
         printf("                                              ");
-        invgotoxy(15,3);
+        invGotoxy(15,3);
         printf("                                              ");
-        invgotoxy(15,2);
+        invGotoxy(15,2);
         printf("Anda menang, yey");
     }
-    invgotoxy(15,3);
-        printf("Masukkan nama : ");
-        int i;
-        for (i=0;i<3;i++) {
-            pl->nama[i] = getch();
-            printf("%c", pl->nama[i]);
-        }
-        saveToLB(*pl);
+    invGotoxy(15,3);
+    printf("Masukkan nama : ");
+    int i;
+    for (i=0;i<3;i++) {
+        pl->nama[i] = getch();
+        printf("%c", pl->nama[i]);
+    }
+    saveToLB(*pl);
 }
+
+/*
+Deskripsi	: Modul untuk memindahkan posisi cursor pada console. X dan y dibalik untuk kebutuhan papan.
+I.S	: Cursor berada di titik asal
+F.S	: Cursor berpindah ke titik yang diinput
+*/
 
 void saveToLB(Player pl){
     Player tPl;
     if ((frec=fopen("hs.dat","ab+"))!=NULL) {
-        // buka file,mode append dalam format biner
+        // open file in binary mode
         tPl.x = pl.x;
         tPl.y = pl.y;
         tPl.skor = pl.skor;
--- gameplay.h
+++ gameplay.h
@@ -10,18 +10,17 @@
 #include <time.h>
 #include <windows.h>
 
+/*
+    Author : Farhan Muhammad Luthfi
+    Date   : 18/12/2022
+*/
+
 typedef struct {
     int x;
     int y;
     int skor;
     char nama[3];
 } Player;
-
-typedef struct {
-    int x;
-    int y;
-    char status;
-} Petak;
 
 /*
 Status:
@@ -30,23 +29,93 @@ X - Terisi
 P - Ditempati player
 */
 
+typedef struct {
+    int x;
+    int y;
+    char status;
+} Petak;
+
+/*
+Deskripsi	: Modul ini sebagai modul utama untuk bermain. Dari sini tampilan akan direset dan berubah menjadi tampilan untuk bermain yang berisi papan catur, timer, skor,
+dan riwayat gerakan player.
+I.S	: Game belum dijalankan
+F.S	: Game dijalankan
+*/
+
 void PlayGame(Player pl);
+
+/*
+Deskripsi	: Modul untuk mengatur posisi awal x dan y player pada papan. Data x dan y dari struct Player diubah lalu melakukan perubahan pada papan sesuai lokasi player
+pada papan. Juga untuk pengaturan timer yang diinginkan, dan inisialisasi skor.
+I.S	: Posisi awal player belum diketahui
+F.S	: Posisi awal player sudah diketahui, timer dan skor sudah diinisialisasi.
+*/
 
 void initializePlayer(Player *pl);
 
+/*
+Deskripsi	: Modul ini digunakan untuk menampilkan papan. Papan akan diprint ulang setiap kali pemain melakukan gerakan untuk memutakhirkan papan sesuai dengan kondisi
+atau state masing-masing petak. Dalam modul ini akan dilakukan pengecekan status dari setiap petak lalu melakukan pencetakan tergantung status tersebut.
+I.S	: Papan catur tidak tampil
+F.S	: Papan catur ditampilkan
+*/
+
 void tampilPapan(Player pl);
 
-void History();
+/*
+Deskripsi	: Menampilkan jejak langkah pemain selama permainan berlangsung.
+I.S	: Jejak terbaru belum ditampilkan.
+F.S	: Jejak terbaru sudah ditampilkan.
+*/
+
+void history();
+
+/*
+Deskripsi	: Menampilkan skor
+I.S	: Skor belum diupdate.
+F.S	: Skor terupdate
+*/
 
 void tampilMisc(Player pl);
 
+/*
+Deskripsi	: Modul untuk pemain memilih petak selanjutnya yang akan ditempati. Dengan menggunakan arrow key, pemain diberikan keluasaan untuk bergerak pada petak manapun.
+Tetapi petak yang bisa ditempati hanya petak yang sesuai dengan langkah kudapada catur (8 kemungkinan).
+I.S	: Posisi player pada suatu petak diketahui.
+F.S	: Posisi player berada pada petak lain dan status petak yang ditempati saat ini berubah.
+*/
+
 void Move(Player *p);
+
+/*
+Deskripsi	: Menyimpan hasil permainan pemain setelah permainan selesai dengan kondisi baik menang maupun kalah.
+I.S	: Jejak terbaru belum ditampilkan.
+F.S	: Jejak terbaru sudah ditampilkan.
+*/
 
 void saveToLB(Player pl);
 
+/*
+Deskripsi	: Modul untuk memindahkan posisi cursor pada console. X dan y dibalik untuk kebutuhan papan.
+I.S	: Cursor berada di titik asal
+F.S	: Cursor berpindah ke titik yang diinput
+*/
+
 void invgotoxy(int x, int y);
 
+/*
+Deskripsi	: Memvalidasi apakah petak yang dituju merupakan langkah kuda.
+I.S	: Tidak diketahui status validasi petak.
+F.S	: Diketahui status validasi petak.
+*/
+
 bool validate(int ix, int iy, int x, int y);
+
+/*
+Deskripsi	: Memvalidasi apakah masih ada petak yang bisa ditempati oleh pemain.
+I.S	: Tidak diketahui status evaluasi petak.
+F.S	: Diketahui status evaluasi petak.
+*/
 
 bool evaluate(int x, int y);
 
